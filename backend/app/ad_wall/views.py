549de9da76cd4a155bbedce76252c83196af9b72
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-03-17 15:09:02 +0300

    Add ordering to AdListView

--- backend/app/ad_wall/views.py
+++ backend/app/ad_wall/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 
 from ad_wall.models import Ad
 from ad_wall.serializers import AdListSerializer
@@ -7,3 +7,5 @@ from ad_wall.serializers import AdListSerializer
 class AdListView(generics.ListAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdListSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["price", "created_on"]
