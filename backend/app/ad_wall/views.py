e8a903aca6b5f6efd669159bb751fb01c57c5487
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-03-18 15:59:00 +0300

    Add AdRetrieve view

--- backend/app/ad_wall/views.py
+++ backend/app/ad_wall/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, filters
 
 from ad_wall.models import Ad
-from ad_wall.serializers import AdListSerializer
+from ad_wall.serializers import AdListSerializer, AdRetrieveSerializer
 
 
 class AdListView(generics.ListAPIView):
@@ -9,3 +9,12 @@ class AdListView(generics.ListAPIView):
     serializer_class = AdListSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ["price", "created_on"]
+
+
+class AdRetrieveView(generics.RetrieveAPIView):
+    queryset = Ad.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.GET.get("fields").lower() in ("true", "1"):
+            return AdRetrieveSerializer
+        return AdListSerializer
