4bc3000899fe1d2d2846e6692effc3ff86dc1f3b
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-03-18 20:20:51 +0300

    Add ads/create endpoint

--- backend/app/ad_wall/urls.py
+++ backend/app/ad_wall/urls.py
@@ -4,5 +4,6 @@ from ad_wall import views
 
 urlpatterns = [
     path("", views.AdListView.as_view()),
-    path("<int:pk>/", views.AdRetrieveView.as_view())
+    path("<int:pk>/", views.AdRetrieveView.as_view()),
+    path("create/", views.AdCreateView.as_view())
 ]
