51763393724baa6655cb2a79189eaf2b3694fe07
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-03-18 15:56:35 +0300

    Add ImageListingField, AdRetrieveSerializer

--- backend/app/ad_wall/serializers.py
+++ backend/app/ad_wall/serializers.py
@@ -7,7 +7,29 @@ class AdListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = (
+            "id",
             "name",
             "primary_image_url",
             "price",
+        )
+
+
+class ImageListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.url
+
+
+class AdRetrieveSerializer(serializers.ModelSerializer):
+    images = ImageListingField(many=True, read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = (
+            "id",
+            "name",
+            "description",
+            "price",
+            "created_on",
+            "primary_image_url",
+            "images",
         )
