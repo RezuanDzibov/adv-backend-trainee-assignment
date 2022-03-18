88c0fe83457882d10c13cae7c815be6b91ae221a
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-03-18 20:21:53 +0300

    Refactor AdRetrieveView and add AdCreateView

--- backend/app/ad_wall/views.py
+++ backend/app/ad_wall/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics, filters
+from rest_framework import generics, filters, response, status
 
 from ad_wall.models import Ad
-from ad_wall.serializers import AdListSerializer, AdRetrieveSerializer
+from ad_wall import serializers
 
 
 class AdListView(generics.ListAPIView):
     queryset = Ad.objects.all()
-    serializer_class = AdListSerializer
+    serializer_class = serializers.AdListSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ["price", "created_on"]
 
@@ -16,5 +16,16 @@ class AdRetrieveView(generics.RetrieveAPIView):
 
     def get_serializer_class(self):
         if self.request.GET.get("fields").lower() in ("true", "1"):
-            return AdRetrieveSerializer
-        return AdListSerializer
+            return serializers.AdRetrieveSerializer
+        return serializers.AdListSerializer
+
+
+class AdCreateView(generics.CreateAPIView):
+    serializer_class = serializers.AdCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = Ad.objects.create(**serializer.data)
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(instance.id, status=status.HTTP_201_CREATED, headers=headers)
